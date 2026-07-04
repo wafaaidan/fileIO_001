1bf7554cab52cab3f051e183450371ea67487e79
wafaaidan <wafa.aidan@gmail.com>
2026-07-04 13:18:19 +0700

    File anggota

--- Anggota.java
+++ Anggota.java
@@ -1,15 +1,15 @@
-abstract class Anggota {
+abstract class Anggota { //abstract class anggota
     private String nama;
     private String id;
     private String kodeJenis;
-    private int jumlahPinjam = 0;
-
+    private int jumlahPinjam = 0; //deklarasi variable
+    //constructor untuk variable 
     public Anggota(String nama, String id, String kodeJenis) {
         this.nama = nama;
         this.id = id;
         this.kodeJenis = kodeJenis;
-    }
-
+    } 
+    //membuat getter dan setter
     public String getNama() {
         return nama;
     }
@@ -24,20 +24,20 @@ abstract class Anggota {
 
     public void tambahPinjam() {
         jumlahPinjam++;
-    }
+    } //method untuk peminjaman buku
 
     public int getJumlahPinjam() {
         return jumlahPinjam;
-    }
+    } 
 }
-
-class Mahasiswa extends Anggota {
+//inheritance dari abstract class
+class Mahasiswa extends Anggota { 
     public Mahasiswa(String nama, String id) {
         super(nama, id, "M");
     }
 }
 
-class Dosen extends Anggota {
+class Dosen extends Anggota { 
     public Dosen(String nama, String id) {
         super(nama, id, "D");
     }
